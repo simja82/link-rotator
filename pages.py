7d6671edc3bc1b4961403ffa7c5677f0c1527e7f
Wedo Pictures <simja.82@hotmail.com>
2023-03-25 22:31:51 -0300

    Pages

--- pages.py
+++ pages.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 import time
 
-list = "https://proyecto-cintianecol.web.app/,https://pov-portfolio.web.app/,https://portfolio-karinsd.web.app/,https://portfolioale-17e1a.web.app/,https://nuevoivonportf.web.app/,https://portfoliofrontend-37881.web.app/,https://porfolio-black-storm.web.app/,https://portfolio-alvaro.web.app/,https://porfolio-ap-ljm.web.app/,https://portfoliojaviertoro.web.app/intro,https://nacargentinaprograma.web.app/,https://portfolio-frontend-nicolas.web.app/,https://portfolio-ezniev.web.app/,https://portfolioelkebarrios.web.app/"
+list = "https://proyecto-cintianecol.web.app/,https://pov-portfolio.web.app/,https://portfolio-karinsd.web.app/,https://portfolioale-17e1a.web.app/,https://nuevoivonportf.web.app/,https://portfoliofrontend-37881.web.app/,https://porfolio-black-storm.web.app/,https://portfolio-alvaro.web.app/,https://porfolio-ap-ljm.web.app/,https://portfoliojaviertoro.web.app/intro,https://nacargentinaprograma.web.app/,https://portfolio-frontend-nicolas.web.app/,https://portfolio-ezniev.web.app/,https://portfolioelkebarrios.web.app/,https://portfolio-frontend-melinda.web.app/"
 tab = ","
 set = 120
 list = list.split(tab)
